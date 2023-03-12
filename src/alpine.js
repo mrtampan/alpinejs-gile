37bc911092a33dae8dd6701f983e214e3ee145ed
achmadrivaldi <mrtampan54@gmail.com>
2023-03-12 14:28:28 +0700

    add transition & pretier

--- src/alpine.js
+++ src/alpine.js
@@ -1,30 +1,30 @@
-import Alpine from 'alpinejs'
- 
-window.Alpine = Alpine
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
 
 Alpine.data('myData', () => ({
-          open: false,
-          toggle() {
-            this.open = ! this.open
-          },
-          firstname: 'Chou',
-          lastname: 'Tzuyu',
-          gender: 'perempuan',
-          hbdOpen: false,
-          hbdTrigger(){
-            this.hbdOpen = ! this.hbdOpen
-          },
-          memberGroup:[
-            { id: 1, nama: 'Jihyo' },
-            { id: 2, nama: 'Sana' },
-            { id: 3, nama: 'Mina' },
-            { id: 4, nama: 'Momo' },
-            { id: 5, nama: 'Chaeyoung' },
-            { id: 6, nama: 'Nayeon' },
-            { id: 7, nama: 'Dahyun' },
-            { id: 8, nama: 'Jeongyeon' },
-        ]
-})
-)
+  open: false,
+  toggle() {
+    this.open = !this.open;
+  },
+  firstname: 'Chou',
+  lastname: 'Tzuyu',
+  gender: 'perempuan',
+  hbdOpen: false,
+  hbdTrigger() {
+    this.hbdOpen = !this.hbdOpen;
+  },
+  buahtext: '',
+  memberGroup: [
+    { id: 1, nama: 'Jihyo' },
+    { id: 2, nama: 'Sana' },
+    { id: 3, nama: 'Mina' },
+    { id: 4, nama: 'Momo' },
+    { id: 5, nama: 'Chaeyoung' },
+    { id: 6, nama: 'Nayeon' },
+    { id: 7, nama: 'Dahyun' },
+    { id: 8, nama: 'Jeongyeon' },
+  ],
+}));
 
-Alpine.start()
+Alpine.start();
